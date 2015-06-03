761a50986276bd659c75d122889cd4bdac3e0fd6
harababurel <srg.pscs@gmail.com>
2015-06-03 17:47:56 +0300

    added example settings.py

--- settings.example.py
+++ settings.example.py
@@ -1,5 +1,21 @@
+from authomatic.providers import oauth2, oauth1
+
 SETTINGS = {
-    'auth': [('cauciuc', 'dezosat')],
+    'auth': [('your_admin_username', 'your_admin_password')],
     'basePic': 'https://graph.facebook.com/%s/picture?width=%s&height=%s',
-    'minVoteWait': 10.0
+    'minVoteWait': 10.0, # time to wait between votes (in case of spam detection)
+    'entriesOnPage': 30,
+    'baseRating': 1200.0,
+    'baseVolatility': 385.0,
+    'fb': {
+        'class_': oauth2.Facebook,
+        'consumer_key': '################',
+        'consumer_secret': '################################',
+        'scope': ['email', 'public_profile', 'user_friends']
+        },
+    'defaultTheme': (
+        'Simplex',
+        '//maxcdn.bootstrapcdn.com/bootswatch/3.3.4/simplex/bootstrap.min.css',
+        'subtle_white_feathers.png'
+        )
 }
