49a5999efbdba99f80c8e2d82f95b4953cce9f26
harababurel <srg.pscs@gmail.com>
2015-06-04 17:00:47 +0300

    adjusted draw pool + highlighted spam votes

--- drawSystem.py
+++ drawSystem.py
@@ -12,7 +12,7 @@ from redirectSolver import solveRedirect
 
 def drawChoices(wantedGender):
     pool = sorted(db.session.query(Person).filter(and_(Person.gender == wantedGender, Person.hidden == False)).all(), key=lambda x: x.games)
-    L, R = sample(pool[:10], 2) # at first, choices are selected from the least voted persons
+    L, R = sample(pool[:50], 2) # at first, choices are selected from the least voted persons
     if randint(1, 2) == 1:      # in order to guarantee variety, each choice has a 50% chance
         L = choice(pool)        # of being re-chosen from the entire person pool
     if randint(1, 2) == 1:
